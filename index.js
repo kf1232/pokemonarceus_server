002a3b4c539c5e8fc1c79aafec9f503be29dccbb
Chance Martin <martin.chancea4@gmail.com>
2022-02-03 13:39:58 -0600

    add db connection
    added remote connection for our mysql db

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const mysql2 = require('mysql2')
+const fs = require('fs')
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+//connect to db
+const db = mysql2.createConnection({
+  host: 'db-mysql-nyc3-60739-do-user-8173291-0.b.db.ondigitalocean.com',
+  port: '25060',
+  user: 'doadmin',
+  password: 'NmlNmEsiRz95ps3t',
+  database: 'defaultdb',
+  ssl : {
+      ca: fs.readFileSync(__dirname + '/ca-certificate.crt'),
+      rejectUnauthorized: false
+  }
 })
+
+db.connect(err => {
+  if(err) {
+      console.log('Connection error message ' + err.message);
+      return;
+  }
+  console.log('MySQL connected')
+})
+
+
+const queryString = 'select * from test';
+
+db.query(queryString, (err, res, fields) => {
+  if (err) {
+      console.log('Error: ' +  err);
+      return;
+  }
+  console.log('yup, thats a table');
+});
+
+db.end;
